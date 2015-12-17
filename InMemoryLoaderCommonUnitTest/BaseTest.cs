64f6bac6e44855258dd8f91bbd355a91a0a2eb38
Kay Stuckenschmidt <kaysta@Linux.athenenet>
2015-12-17 14:52:22 +0000

    Add Unit-Test Solution

---
+++ InMemoryLoaderCommonUnitTest/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using InMemoryLoader;
+using InMemoryLoaderBase;
+using InMemoryLoaderCommon;
+using log4net;
+
+namespace InMemoryLoaderCommonUnitTest
+{
+	[TestFixture ()]
+	public class BaseTest
+	{
+		private static readonly ILog log = LogManager.GetLogger (typeof(BaseTest));
+		private static AppBase appBase = AppBase.Instance;
+
+		[Test ()]
+		public void BaseTestCase ()
+		{
+			try {
+
+				Assert.IsNotNull(appBase.ComponentLoader);
+				Assert.IsNotNull(appBase.CommonComponentLoader);
+
+			} catch (Exception ex) {
+				log.FatalFormat (ex.ToString());
+			}
+		}
+	}
+}
+
