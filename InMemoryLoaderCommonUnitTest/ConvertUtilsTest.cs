91a507345e380e6716f6d38f776ce2dd43deba9c
Kay Stuckenschmidt <kaysta@Linux.athenenet>
2015-12-21 01:19:16 +0000

    Some new Tests

---
+++ InMemoryLoaderCommonUnitTest/ConvertUtilsTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using InMemoryLoader;
+using InMemoryLoaderBase;
+using InMemoryLoaderCommon;
+using log4net;
+
+namespace InMemoryLoaderCommonUnitTest
+{
+	[TestFixture ()]
+	public class ConvertUtilsTest
+	{
+		/// <summary>
+		/// The log.
+		/// </summary>
+		private static readonly ILog log = LogManager.GetLogger (typeof(ConvertUtilsTest));
+
+		[Test ()]
+		public void ConvertUtilsBoolTestCase ()
+		{
+			try {
+				log.InfoFormat ("{0}", "ConvertUtilsBoolTestCase");
+
+				// Is true
+				var isTrue1 = ConvertUtilsBoolests.ConvertBoolTest1();
+				Assert.IsTrue(isTrue1);
+				log.InfoFormat ("ConvertBoolTest1 = true is {0}", isTrue1);
+
+				// Is false
+				var isTrue2 = ConvertUtilsBoolests.ConvertBoolTest2();
+				Assert.IsFalse(isTrue2);
+				log.InfoFormat ("ConvertBoolTest1 = false is {0}", isTrue2);
+
+
+			} catch (Exception ex) {
+				log.FatalFormat (ex.ToString());
+			}
+		}
+	}
+}
+
---
+++ InMemoryLoaderCommonUnitTest/Test/ConvertUtilsBoolests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using InMemoryLoader;
+using InMemoryLoaderBase;
+using InMemoryLoaderCommon;
+using log4net;
+using System.Globalization;
+using System.Linq;
+
+namespace InMemoryLoaderCommonUnitTest
+{
+	public class ConvertUtilsBoolests
+	{
+		/// <summary>
+		/// The app base.
+		/// </summary>
+		private static AppBase appBase = AppBase.Instance;
+		/// <summary>
+		/// The convert utils.
+		/// </summary>
+		private static IDynamicClassSetup convertUtils = appBase.CommonComponentLoader.ConvertComponent;
+
+		/// <summary>
+		/// The is string true.
+		/// </summary>
+		private const string isStringTrue = "1";
+		/// <summary>
+		/// The is string false.
+		/// </summary>
+		private const string isStringFalse = "0";
+		/// <summary>
+		/// The is char true.
+		/// </summary>
+		private const char isCharTrue = '1';
+		/// <summary>
+		/// The is char false.
+		/// </summary>
+		private const char isCharFalse = '0';
+
+		/// <summary>
+		/// Converts the bool = true
+		/// </summary>
+		/// <returns><c>true</c>, if bool test1 was converted, <c>false</c> otherwise.</returns>
+		public static bool ConvertBoolTest1 ()
+		{
+			try {
+				object[] paramArg = { isStringTrue };
+
+				var result = appBase.ComponentLoader.InvokeMethod (convertUtils.Assembly, convertUtils.Class, "StringToBoolean", paramArg);
+				var isTrue = Convert.ToBoolean (result);
+
+				return isTrue;
+			} catch (Exception ex) {
+				throw ex;
+			}		
+		}
+
+		/// <summary>
+		/// Converts the bool = false
+		/// </summary>
+		/// <returns><c>true</c>, if bool test2 was converted, <c>false</c> otherwise.</returns>
+		public static bool ConvertBoolTest2 ()
+		{
+			try {
+				object[] paramArg = { isStringFalse };
+
+				var result = appBase.ComponentLoader.InvokeMethod (convertUtils.Assembly, convertUtils.Class, "StringToBoolean", paramArg);
+				var isTrue = Convert.ToBoolean (result);
+
+				return isTrue;
+			} catch (Exception ex) {
+				throw ex;
+			}		
+		}
+
+
+
+	}
+}
+
