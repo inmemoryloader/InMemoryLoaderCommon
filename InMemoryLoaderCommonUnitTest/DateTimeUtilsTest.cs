62ff88594be0549f582e5915a91f3a2c227a250d
Kay Stuckenschmidt <kaysta@Linux.athenenet>
2016-01-11 19:21:27 +0000

    DateTime UnitTests

---
+++ InMemoryLoaderCommonUnitTest/DateTimeUtilsTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using InMemoryLoader;
+using InMemoryLoaderBase;
+using InMemoryLoaderCommon;
+using log4net;
+
+namespace InMemoryLoaderCommonUnitTest
+{
+	[TestFixture ()]
+	public class DateTimeUtilsTest
+	{
+		/// <summary>
+		/// The log.
+		/// </summary>
+		private static readonly ILog log = LogManager.GetLogger (typeof(CryptUtilsTest));
+
+
+		[Test ()]
+		public void DateTimeUtilsCalendarWeekTestCase ()
+		{
+			try {
+				log.InfoFormat ("{0}", "DateTimeUtilsCalendarWeekTestCase");
+
+				// Date is 01.01.2016
+
+				var returnCalendarWeek = DateTimeUtilsCalendarWeekTests.GetCalendarWeekTest();
+
+				log.InfoFormat ("GetCalendarWeekTest is Year {0} and Week {1}", returnCalendarWeek.Year, returnCalendarWeek.Week);
+
+			} catch (Exception ex) {
+				log.FatalFormat (ex.ToString ());
+			}
+		}
+	}
+}
+
---
+++ InMemoryLoaderCommonUnitTest/Test/DateTimeUtilsCalendarWeekTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using InMemoryLoader;
+using InMemoryLoaderBase;
+using InMemoryLoaderCommon;
+using log4net;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+using System.Text;
+using PowerUpDateTimeUtils;
+
+namespace InMemoryLoaderCommonUnitTest
+{
+	public class DateTimeUtilsCalendarWeekTests
+	{
+		/// <summary>
+		/// The app base.
+		/// </summary>
+		private static AppBase appBase = AppBase.Instance;
+		/// <summary>
+		/// The crypt utils.
+		/// </summary>
+		private static IDynamicClassSetup dateTimeUtils = appBase.CommonComponentLoader.DateTimeComponent;
+		/// <summary>
+		/// The convert utils.
+		/// </summary>
+		private static IDynamicClassSetup convertUtils = appBase.CommonComponentLoader.ConvertComponent;
+
+
+		private static DateTime FirstWeekInThisYear ()
+		{
+			try {
+				var dateString = "01.01.2016";
+				object[] paramDateString = { dateString };
+
+				var returnDate = appBase.ComponentLoader.InvokeMethod (convertUtils.Assembly, convertUtils.Class, "NormalizeDate", paramDateString);
+
+				return (DateTime)returnDate;
+			} catch (Exception ex) {
+				throw ex;
+			}
+		}
+
+		public static CalendarWeek GetCalendarWeekTest ()
+		{
+			try {
+				var varDate = FirstWeekInThisYear();
+				object[] paramDate = { varDate };
+
+				var calendarWeek = appBase.ComponentLoader.InvokeMethod (dateTimeUtils.Assembly, dateTimeUtils.Class, "GetCalendarWeek", paramDate);
+
+				return (CalendarWeek)calendarWeek;
+			} catch (Exception ex) {
+				throw ex;
+			}		
+		}
+	}
+}
+
