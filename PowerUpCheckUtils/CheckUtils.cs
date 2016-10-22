f92379dd240d72e6db9cc38845cf091e8ebe49b7
Kay Stuckenschmidt <kay.stuckenschmidt@maxomedia.ch>
2016-10-22 14:22:41 +0200

    some plastic surgery

--- PowerUpCheckUtils/CheckUtils.cs
+++ PowerUpCheckUtils/CheckUtils.cs
@@ -1,27 +1,24 @@
-﻿using System;
+﻿using InMemoryLoaderBase;
 using log4net;
-using InMemoryLoaderBase;
-using System.Globalization;
 
 namespace PowerUpCheckUtils
 {
-	/// <summary>
-	/// Check utils.
-	/// </summary>
-	public partial class CheckUtils : AbstractPowerUpComponent
-	{
-		/// <summary>
-		/// The log.
-		/// </summary>
-		private static readonly ILog log = LogManager.GetLogger (typeof(CheckUtils));
+    /// <summary>
+    /// Check utils.
+    /// </summary>
+    public partial class CheckUtils : AbstractPowerUpComponent
+    {
+        /// <summary>
+        /// The log.
+        /// </summary>
+        private static readonly ILog log = LogManager.GetLogger(typeof(CheckUtils));
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="PowerUpCheckUtils.CheckUtils"/> class.
-		/// </summary>
-		public CheckUtils ()
-		{
-			log.DebugFormat ("Create a new instance of Type: {0}", this.GetType ().ToString ());
-		}
-	}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PowerUpCheckUtils.CheckUtils"/> class.
+        /// </summary>
+        public CheckUtils()
+        {
+            log.DebugFormat("Create a new instance of Type: {0}", this.GetType().ToString());
+        }
+    }
 }
-
