245da922f0f34fefe5a93a7a4eb6843110451e9d
responsive kaysta <me@responsive-kaysta.ch>
2017-10-29 00:55:06 +0200

    code cleanup, reorganised usings, documentation

--- PowerUpCryptUtils/Properties/AssemblyInfo.cs
+++ PowerUpCryptUtils/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("responsive kaysta")]
 [assembly: AssemblyTrademark("responsive kaysta")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.2.9.1")]
+[assembly: AssemblyVersion("0.2.9.2")]
 [assembly: ComVisible(false)]
--- PowerUpFileSystemUtils/Properties/AssemblyInfo.cs
+++ PowerUpFileSystemUtils/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle ("PowerUpFileSystemUtils")]
+[assembly: AssemblyTitle("PowerUpFileSystemUtils")]
 [assembly: AssemblyDescription("InMemoryLoader ist eine in C# geschriebene Funktionsbibliothek welche das dynamische Laden von .NET Assemblies zur Laufzeit ermöglicht.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("responsive kaysta")]
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("responsive kaysta")]
 [assembly: AssemblyTrademark("responsive kaysta")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.2.9.0")]
+[assembly: AssemblyVersion("0.2.9.1")]
 [assembly: ComVisible(false)]
